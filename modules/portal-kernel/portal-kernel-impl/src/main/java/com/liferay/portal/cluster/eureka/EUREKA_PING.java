d40572dbfb7fed813bc6e7e393db9e9641362214
dancewing <qbaojian@163.com>
2018-01-27 21:22:08 +0800

    add eureka support

---
+++ modules/portal-kernel/portal-kernel-impl/src/main/java/com/liferay/portal/cluster/eureka/EUREKA_PING.java
@@ -0,0 +1,95 @@
+package com.liferay.portal.cluster.eureka;
+
+import org.jgroups.Address;
+import org.jgroups.Event;
+import org.jgroups.Message;
+import org.jgroups.conf.ClassConfigurator;
+import org.jgroups.protocols.*;
+import org.jgroups.stack.IpAddress;
+import org.jgroups.util.NameCache;
+import org.jgroups.util.Responses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EUREKA_PING extends FILE_PING {
+
+    private static Map<String, PingData> datas = new HashMap<>();
+
+    private static final Logger _log = LoggerFactory.getLogger(EUREKA_PING.class);
+
+    private final DiscoveryClient discoveryClient;
+
+    private final TCP tcp;
+
+    public EUREKA_PING(DiscoveryClient discoveryClient, TCP tcp) {
+        this.discoveryClient = discoveryClient;
+        this.tcp = tcp;
+    }
+
+    static {
+        ClassConfigurator.addProtocol((short) 600, EUREKA_PING.class); // ID needs to be unique
+    }
+
+    @Override
+    public boolean isDynamic() {
+        return true;
+    }
+
+    @Override
+    protected void readAll(List<Address> members, String clustername, Responses responses) {
+        _log.info("Get local TCP port :" + this.tcp.getBindPort());
+
+        List<ServiceInstance> instances = discoveryClient.getInstances(clustername);
+        for (ServiceInstance instance : instances) {
+            try {
+                String physicalAddr =  instance.getHost() + ":" + instance.getPort();
+
+                String key = clustername + "_" + physicalAddr;
+
+                PingData data = readData(key);
+
+                if (data!=null && (members == null || members.contains(data.getAddress()) ))
+                    responses.addResponse(data, data.isCoord());
+                if(data!=null && local_addr != null && !local_addr.equals(data.getAddress()))
+                    addDiscoveryResponseToCaches(data.getAddress(), data.getLogicalName(), data.getPhysicalAddr());
+
+            } catch (Exception e) {
+                _log.error(e.getMessage(),e);
+            }
+        }
+    }
+
+    @Override
+    protected void write(List<PingData> list, String clustername) {
+        if (discoveryClient.getInstances(clustername).size()>0) {
+            for(PingData data: list) {
+                String physicalAddr =  data.getPhysicalAddr().printIpAddress();
+                String key = clustername + "_" + physicalAddr;
+                _log.info("ping data key: " + key);
+                datas.put(key, data);
+            }
+        }
+    }
+
+    @Override
+    protected void remove(String clustername, Address addr) {
+
+    }
+
+    private PingData readData(String key) {
+        return datas.get(key);
+    }
+
+    @Override
+    public void destroy() {
+        datas.clear();
+        super.destroy();
+    }
+}
---
+++ modules/portal-kernel/portal-kernel-impl/src/main/java/com/liferay/portal/cluster/eureka/EurekaClusterRequestReceiver.java
@@ -0,0 +1,256 @@
+package com.liferay.portal.cluster.eureka;
+
+import com.liferay.portal.cluster.AddressImpl;
+import com.liferay.portal.cluster.BaseReceiver;
+import com.liferay.portal.cluster.ClusterRequestReceiver;
+import com.liferay.portal.kernel.cluster.*;
+import com.liferay.portal.kernel.util.MethodHandler;
+import org.jgroups.Message;
+import org.jgroups.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EurekaClusterRequestReceiver extends BaseReceiver {
+
+    public EurekaClusterRequestReceiver(EurekaClusterExecutorImpl clusterExecutorImpl) {
+        _clusterExecutorImpl = clusterExecutorImpl;
+    }
+
+    @Override
+    protected void doReceive(Message message) {
+        Object obj = retrievePayload(message);
+
+        if (obj == null) {
+            return;
+        }
+
+        Address sourceAddress = new AddressImpl(message.getSrc());
+
+        if (sourceAddress.equals(
+                _clusterExecutorImpl.getLocalClusterNodeAddress())) {
+
+            boolean isProcessed = processLocalMessage(obj);
+
+            if (isProcessed) {
+                return;
+            }
+        }
+
+        if (obj instanceof ClusterRequest) {
+            ClusterRequest clusterRequest = (ClusterRequest)obj;
+
+            processClusterRequest(clusterRequest, sourceAddress);
+        }
+        else if (obj instanceof ClusterNodeResponse) {
+            ClusterNodeResponse clusterNodeResponse = (ClusterNodeResponse)obj;
+
+            processClusterResponse(clusterNodeResponse, sourceAddress);
+        }
+        else if (_log.isWarnEnabled()) {
+            _log.warn(
+                    "Unable to process message content of type " + obj.getClass());
+        }
+    }
+
+    @Override
+    protected void doViewAccepted(View oldView, View newView) {
+        List<Address> departAddresses = getDepartAddresses(oldView, newView);
+        List<Address> newAddresses = getNewAddresses(oldView, newView);
+
+        if (!newAddresses.isEmpty()) {
+            _clusterExecutorImpl.sendNotifyRequest();
+        }
+
+        if (!departAddresses.isEmpty()) {
+            _clusterExecutorImpl.memberRemoved(departAddresses);
+        }
+    }
+
+    protected List<Address> getDepartAddresses(View oldView, View newView) {
+        List<org.jgroups.Address> currentJGroupsAddresses =
+                newView.getMembers();
+        List<org.jgroups.Address> lastJGroupsAddresses = oldView.getMembers();
+
+        List<org.jgroups.Address> departJGroupsAddresses =
+                new ArrayList<org.jgroups.Address>(lastJGroupsAddresses);
+
+        departJGroupsAddresses.removeAll(currentJGroupsAddresses);
+
+        if (departJGroupsAddresses.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Address> departAddresses = new ArrayList<Address>(
+                departJGroupsAddresses.size());
+
+        for (org.jgroups.Address departJGroupsAddress :
+                departJGroupsAddresses) {
+
+            Address departAddress = new AddressImpl(departJGroupsAddress);
+
+            departAddresses.add(departAddress);
+        }
+
+        return departAddresses;
+    }
+
+    protected List<Address> getNewAddresses(View oldView, View newView) {
+        List<org.jgroups.Address> currentJGroupsAddresses =
+                newView.getMembers();
+        List<org.jgroups.Address> lastJGroupsAddresses = oldView.getMembers();
+
+        List<org.jgroups.Address> newJGroupsAddresses =
+                new ArrayList<org.jgroups.Address>(currentJGroupsAddresses);
+
+        newJGroupsAddresses.removeAll(lastJGroupsAddresses);
+
+        if (newJGroupsAddresses.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Address> newAddresses = new ArrayList<Address>(
+                newJGroupsAddresses.size());
+
+        for (org.jgroups.Address newJGroupsAddress : newJGroupsAddresses) {
+            Address newAddress = new AddressImpl(newJGroupsAddress);
+
+            newAddresses.add(newAddress);
+        }
+
+        return newAddresses;
+    }
+
+    protected void handleResponse(
+            Address address, ClusterRequest clusterRequest, Object returnValue,
+            Exception exception) {
+
+        ClusterNodeResponse clusterNodeResponse =
+                _clusterExecutorImpl.generateClusterNodeResponse(
+                        clusterRequest, returnValue, exception);
+
+        try {
+            _clusterExecutorImpl.sendJGroupsMessage(
+                    _clusterExecutorImpl.getControlChannel(),
+                    (org.jgroups.Address)address.getRealAddress(),
+                    clusterNodeResponse);
+        }
+        catch (Exception e) {
+            _log.error(
+                    "Unable to send response message " + clusterNodeResponse, e);
+        }
+        catch (Throwable t) {
+            _log.error(t.getMessage(), t);
+        }
+    }
+
+    protected void processClusterRequest(
+            ClusterRequest clusterRequest, Address sourceAddress) {
+
+        ClusterMessageType clusterMessageType =
+                clusterRequest.getClusterMessageType();
+
+        if (clusterMessageType.equals(ClusterMessageType.NOTIFY) ||
+                clusterMessageType.equals(ClusterMessageType.UPDATE)) {
+
+            _clusterExecutorImpl.memberJoined(
+                    sourceAddress, clusterRequest.getOriginatingClusterNode());
+
+            if (clusterMessageType.equals(ClusterMessageType.NOTIFY)) {
+                handleResponse(sourceAddress, clusterRequest, null, null);
+            }
+
+            return;
+        }
+
+        MethodHandler methodHandler = clusterRequest.getMethodHandler();
+
+        Object returnValue = null;
+        Exception exception = null;
+
+        if (methodHandler != null) {
+            try {
+                ClusterInvokeThreadLocal.setEnabled(false);
+
+                returnValue = methodHandler.invoke(true);
+            }
+            catch (Exception e) {
+                exception = e;
+
+                _log.error("Unable to invoke method " + methodHandler, e);
+            }
+            finally {
+                ClusterInvokeThreadLocal.setEnabled(true);
+            }
+        }
+        else {
+            exception = new ClusterException(
+                    "Payload is not of type " + MethodHandler.class.getName());
+        }
+
+        if (!clusterRequest.isFireAndForget()) {
+            handleResponse(
+                    sourceAddress, clusterRequest, returnValue, exception);
+        }
+    }
+
+    protected void processClusterResponse(
+            ClusterNodeResponse clusterNodeResponse, Address sourceAddress) {
+
+        ClusterMessageType clusterMessageType =
+                clusterNodeResponse.getClusterMessageType();
+
+        if (clusterMessageType.equals(ClusterMessageType.NOTIFY)) {
+            _clusterExecutorImpl.memberJoined(
+                    sourceAddress, clusterNodeResponse.getClusterNode());
+
+            return;
+        }
+
+        String uuid = clusterNodeResponse.getUuid();
+
+        FutureClusterResponses futureClusterResponses =
+                _clusterExecutorImpl.getExecutionResults(uuid);
+
+        if (futureClusterResponses == null) {
+            if (_log.isInfoEnabled()) {
+                _log.info("Unable to find response container for " + uuid);
+            }
+
+            return;
+        }
+
+        if (futureClusterResponses.expectsReply(sourceAddress)) {
+            futureClusterResponses.addClusterNodeResponse(clusterNodeResponse);
+        }
+        else {
+            if (_log.isWarnEnabled()) {
+                _log.warn("Unknown uuid " + uuid + " from " + sourceAddress);
+            }
+        }
+    }
+
+    protected boolean processLocalMessage(Object message) {
+        if (message instanceof ClusterRequest) {
+            ClusterRequest clusterRequest = (ClusterRequest)message;
+
+            if (clusterRequest.isSkipLocal()) {
+                return true;
+            }
+        }
+
+        if (_clusterExecutorImpl.isShortcutLocalMethod()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static final Logger _log = LoggerFactory.getLogger(
+            ClusterRequestReceiver.class);
+
+    private EurekaClusterExecutorImpl _clusterExecutorImpl;
+}
